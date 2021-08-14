a2dc0dd17557c50b17f784ca14c97c187caa633d
Ryusuke Aida <85981476+raida42@users.noreply.github.com>
2021-08-14 15:23:03 +0900

    Update main.c

--- main.c
+++ main.c
@@ -9,27 +9,10 @@ int main()
 	char *p;
 	p = "HELLO 42";
 	//cspdiuxX
-	// i = ft_printf("%010 ##000c\n", 'A');
-	// i = ft_printf("%05v\n");
-	i = ft_printf("|%+10.0d|\n", 0);
-	// i = ft_printf("%c %3s %p %d %i %u %#x %#X %010hjl qtzqqty'hj-lqtzL30h@|$-abcdef\n", 'A', p, p, INT_MAX, INT_MIN, UINT64_MAX, 0, 0);
-	// i = ft_printf("%-10 ## +'hjlqt9zLhjlqtz&qh$abcdef\n");
+	i = ft_printf("%c %s %p %d %i %u %x %X %%", 'A', p, p, INT_MAX, INT_MIN, UINT64_MAX, INT_MAX, INT_MAX);
 	ft_printf("return ; %d\n", i);
-	j = printf("|%+10.0d|\n", 0);
-	j =    printf("%c %3s %p %d %i %u %#x %#X %010hjl qtzqqty'hj-lqtzL30h@|$-abcdef\n", 'A', p, p, INT_MAX, INT_MIN, UINT64_MAX, 0, 0);
-	// j =    printf("%*%\n", INT_MAX-1);
-	// j =    printf("%010 ##000c\n", 'A');
-	j =    printf("%-10 ## +'hjlqt9zLhjlqtz&qh$abcdef");
-	// j = printf("%05v\n");
+	j =    printf("%c %s %p %d %i %u %x %X %%", 'A', p, p, INT_MAX, INT_MIN, UINT64_MAX, INT_MAX, INT_MAX);
 	printf("return ; %d\n", j);
-	// system("leaks a.out");
-	// int max = int_max;
-	// int min = int_min;
-	// printf("\n\n%d", max);
-	// adiouxefgcspnm変換指定子
-	// hjlqtz長さ修飾子 
-	// bkrwy特殊
-	// 超特殊 'v'
-	return 0;
+	return (0);
 }
 
